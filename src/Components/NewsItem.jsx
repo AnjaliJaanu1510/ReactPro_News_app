439f9d9256b4aaccfaf4691834efd42892e0c3ad
Anjali Adhikari <142712776+AnjaliJaanu1510@users.noreply.github.com>
2023-11-04 18:12:35 +0530

    first commit

---
+++ src/Components/NewsItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import image from '../assets/news.png'
+
+const NewsItem=({title,description,src,url})=> {
+  return (
+    <>
+ <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"350px"}}>
+  <img src={src? src:image} style={{height:"250px",width:"325px"}} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title.slice(0,50)}</h5>
+    <p className="card-text">{description? description.slice(0,100):"A meta description  short summary of your page and generates the brief snippet you see underneath a site’s title in Google’s organic"}</p>
+    <a href={url} className="btn btn-primary">Read More</a>
+  </div>
+</div>
+
+    </>
+  );
+}
+
+export default NewsItem;
